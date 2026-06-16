623fae258719b1056b0821e4ed1c0f05ea0556fe
Jovin Joseph <jovinjoseph171@gmail.com>
2026-06-16 12:52:48 +0400

    Setup basic web server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+
+    mux := http.NewServeMux()
+    server := http.Server{
+        Addr: ":8080", 
+        Handler: mux,
+    }
+    err := server.ListenAndServe()
+
+    if err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
