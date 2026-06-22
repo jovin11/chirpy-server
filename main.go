29e839795b428682d1c30c01408366623fe108c3
Jovin Joseph <jovinjoseph171@gmail.com>
2026-06-22 20:43:22 +0400

    Add sqlc setup and postgres database wiring

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+    "os"
 	"log"
 	"net/http"
 	"sync/atomic"
+    "database/sql"
+    _ "github.com/lib/pq"
+    "github.com/joho/godotenv"
+    "github.com/jovinjoseph/chirpy-server/internal/database"
 )
 
 // apiConfig tracks stateful data across HTTP handlers.
 // It is intended to be initialized once and shared between routes.
 type apiConfig struct {
 	fileserverHits atomic.Int32
+    dbQueries *database.Queries
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	apiCfg := apiConfig{}
+    godotenv.Load()
+    dbURL := os.Getenv("DB_URL")
+    db, err := sql.Open("postgres", dbURL)
+    if err != nil {
+	    log.Fatalf("Database connection failed: %v\n", err)
+    }
+    dbQueries := database.New(db)
+
+	apiCfg := apiConfig{dbQueries: dbQueries}
 	mux := http.NewServeMux()
 
 	// The /app/ prefix is stripped so the file server can look up files
@@ -35,10 +49,6 @@ func main() {
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
-
-
-
-
